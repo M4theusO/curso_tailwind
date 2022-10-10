4b0c1e95643f9b1a9c6f9d6640b6bf7d5a3e8c86
Matheus de Oliveira <matheus_oliveira8@estudante.sc.senai.br>
2022-10-10 16:27:51 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'media',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        brand: '#c542f5'
+      }
+    },
+  },
+  plugins: [],
+}
